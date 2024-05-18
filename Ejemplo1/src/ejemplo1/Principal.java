9103fcd1c524b560063b1c1bf7bf3a50643f6c76
jadg13 <jadg13@hotmail.com>
2024-05-18 11:39:27 -0600

    Sesion 18 de mayo

---
+++ Ejemplo1/src/ejemplo1/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplo1;
+
+/**
+ *
+ * @author jadg13
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo.");
+        System.out.print("Desde la UNIVALLE");
+        System.out.println(" - Managua, Nicaragua");
+    }
+    
+}
